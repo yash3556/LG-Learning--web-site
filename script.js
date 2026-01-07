ffd635fdefddf76aed46178dae9301253441399f
Yash Kushwaha <yashkush660@gmail.com>
2026-01-07 13:36:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+    const menuToggle = document.getElementById("menuToggle");
+    const nav = document.querySelector(".nav_item");
+
+    menuToggle.addEventListener("click", () => {
+        nav.classList.toggle("active");
+    });
+
+
+
+     
